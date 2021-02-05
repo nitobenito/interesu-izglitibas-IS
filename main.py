0b118a6511974a497a7aec83aa255cfd9a84fe61
Arturs Teihmanis <arturs.teihmanis@gmail.com>
2021-02-05 14:17:50 +0000

    copypaste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, jsonify
-from datetime import datetime
-
+from flask import Flask, render_template, jsonify,json ,request, make_response
+import dati
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII']=False
@@ -10,32 +9,14 @@ app.config['JSON_AS_ASCII']=False
 def index():
   return render_template('index.html')
 
-@app.route('/tabula')
-def uzTabulu():
-  return render_template('tabula.html')
-
-@app.route('/pulcinu_saraksts')
-def pulcSar():
-  return render_template('pulcinu_saraksts.html')
-
 @app.route('/skolenu_izvelne')
-def time():
-  now = datetime.now() # current date and time
-  date_time = now.strftime("%d/%m/%Y, %H:%M:%S")
-  return render_template('skolenu_izvelne.html', date_time=date_time)
-
 def skIzvele():
   return render_template('skolenu_izvelne.html')
-
-@app.route('/pub_data')
-def pub_dati():
-  return render_template('pub_data.html')
 
 @app.route('/IIC')
 def iic():
   return render_template('IIC.html')  
-
-
+  
 @app.route('/vecaku_pieteikums')
 def vPieteikums():
   return render_template('vecaku_pieteikums.html')
